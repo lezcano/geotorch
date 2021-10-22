7235df90a17fcf7f7aa18bb96df3d3c91a7bd53a
lezcano <lezcano-93@hotmail.com>
2021-10-22 09:26:30 +0100

    Updated readme and setup

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     keywords=["Constrained Optimization", "Optimization on Manifolds", "Pytorch"],
     packages=find_packages(),
     python_requires=">=3.5",
-    install_requires=["torch>=1.5"],
+    install_requires=["torch>=1.8"],
     extras_require={"dev": DEV_REQUIRES, "test": TEST_REQUIRES},
 )
